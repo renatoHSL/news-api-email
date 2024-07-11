2fc89137eaaff883f521f659f6042281c88b101c
Renato <renato92.hora@gmail.com>
2024-07-11 10:36:39 -0300

    Add links, specify language

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import requests
+from send_email import send_email
+
+topic = "tesla"
 
 api_key = "7ce2552ba0704fb19975b4c33863a065"
-url = "https://newsapi.org/v2/everything?q=tesla&" \
-      "from=2024-06-10&sortBy=publishedAt&apiKey=" \
-      "7ce2552ba0704fb19975b4c33863a065"
+url = "https://newsapi.org/v2/everything?" \
+       f"q={topic}&" \
+       "from=2024-07-10&" \
+       "sortBy=publishedAt&" \
+       "apiKey=7ce2552ba0704fb19975b4c33863a065&" \
+       "language=en"
 
 request = requests.get(url)
 content = request.json()
 
-
+body = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article['description'])
+    if article["title"] is not None:
+        body = "Subject: Today's news" \
+                + body + article["title"] + "\n" \
+                + article["description"] \
+                + "\n" + article["url"] + 2 * "\n"
 
-
-
+body = body.encode("utf-8")
+send_email(message=body)
---
+++ send_email.py
@@ -0,0 +1,18 @@
+import smtplib
+import ssl
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "app8flask@gmail.com"
+    password = "password"
+
+    receiver = "app8flask@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
